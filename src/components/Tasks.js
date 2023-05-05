398fe2498a180b86a36c6c252bd82075ca9b3c54
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-05 05:58:09 +0300

    Tasks Component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
+const tasks = [
+  {
+    id : 1,
+    text: "Doctors Appointment",
+    day: "5th feb  at 2:30pm",
+    reminder: true,
+  },
+  {
+    id : 2,
+    text: "Load the dryer",
+    day: "5th feb  at 1:30pm",
+    reminder: false,
+  },
+  {
+    id : 3,
+    text: "Cook lasagna",
+    day: "5th feb  at 11:30am",
+    reminder: true,
+  },
+  {
+    id : 4,
+    text: "Grocery Shopping",
+    day: "5th feb  at 8:30am",
+    reminder: false,
+  }
+]
+
 const Tasks = () => {
   return (
-    <div>Tasks</div>
+    // We are going to map the array inside the tags
+    <React.Fragment>
+    <>
+       {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+    </>
+    </React.Fragment>
   )
 }
 
