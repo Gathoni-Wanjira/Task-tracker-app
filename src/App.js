8e1752c23096e1fc01285108657d86ca98e24ab9
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-05 17:03:38 +0300

    Delete Task

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ import Tasks from './components/Tasks';
 
 
 const App = () => {
-  const [tasks, setTasks] = useState (
+  const [tasks, setTasks] = useState(
     [
       {
-        id : 1,
+        id: 1,
         text: "Doctors Appointment",
         day: "1st feb  at 2:30pm",
         reminder: true,
       },
       {
-        id : 2,
+        id: 2,
         text: "Load the dryer",
         day: "4th feb  at 1:30pm",
         reminder: false,
       },
       {
-        id : 3,
+        id: 3,
         text: "Cook lasagna",
         day: "6th feb  at 11:30am",
         reminder: true,
       },
       {
-        id : 4,
+        id: 4,
         text: "Grocery Shopping",
         day: "7th feb  at 8:30am",
         reminder: false,
@@ -35,13 +35,19 @@ const App = () => {
     ]
   )
 
+
+  // Delete task function
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     // You can only have one main parent element.
     <div className="container">
-    < Header />
-    < Tasks tasks={tasks} />
+      < Header />
+      < Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
-  
+
   )
 }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,20 @@
 // We are styling the individual task from the tasks component.
 // We are also importing react icons and add it to the h3 tag as a component
 import React from 'react'
-import {FaTimes} from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task }) => {
-  return (
-    <React.Fragment>
-        <div className='task'>
-            <h3>
-                {task.text} <FaTimes style={{color: "red", cursor: "pointer"} }/>
+const Task = ({ task, onDelete }) => {
+    return (
+        <React.Fragment>
+            <div className='task'>
+                <h3>
+                    {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }}
+                        onClick={() => onDelete(task.id)} />
                 </h3>
-            <p>{task.day}</p>
-        </div>
-    </React.Fragment>
-  )
+                <p>{task.day}</p>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default Task
